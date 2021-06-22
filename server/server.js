9c8dab777f777e6056d46d438585034ed249a58d
Casey <casey.a.whitfield@gmail.com>
2021-06-22 11:33:19 -0400

    allow Cross-Origin-Resource-Sharing

--- server/server.js
+++ server/server.js
@@ -7,12 +7,25 @@ const app = express()
 const port = 3000
 
 
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept');
+	
+	next();
+
+});
+
 app.use(bodyParser.json())
 app.use(
 	bodyParser.urlencoded({
 		extended: true,
 	})
 )
+
+
 
 
 app.get('/', (request, response) => {
